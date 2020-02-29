c9b35d50426713f68c7aed0fa96168f85cbccbf4
Lucas Toole <lucas@ltoole.com>
2020-02-28 19:47:23 -0600

    Added Config file support
    Still needs further work, most importantly defining multiple servers,
    but for now I'm happy.

--- matchbot/matchbot.go
+++ matchbot/matchbot.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net"
 	"fmt"
+
+	"github.com/pelletier/go-toml"
 )
 
 type PassedLogs struct {
@@ -17,18 +19,48 @@ type CommandInfo struct {
 	cmd string
 }
 
+type BotInfo struct {
+	addr string
+	name string
+}
+
 type ServerInfo struct {
 	conn net.Conn
 	addr string
+	rconPass string
 	ready int8 /* 0 - none, 1 - CT, 2 - T, 3 - Both */
 	isInit bool
 	isWarmup bool
 	isPaused bool
 }
 
+var serv *ServerInfo = new(ServerInfo)
+var bot *BotInfo = new(BotInfo)
+
+func Init_Server() {
+	config, err := toml.LoadFile("config.toml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	bot.name = config.Get("Bot.name").(string)
+	bot.addr = (config.Get("Bot.ip").(string) + ":" + config.Get("Bot.port").(string))
+
+	serv.addr = (config.Get("Server.ip").(string) + ":" + config.Get("Server.port").(string))
+	serv.rconPass = config.Get("Server.rconpass").(string)
+
+	fmt.Println("Bot name: " + bot.name)
+	fmt.Println("Bot address: " + bot.addr)
+
+	fmt.Println("Server address: " + serv.addr)
+	fmt.Println("Server Rcon Pass: " + serv.rconPass)
+	
+}
+
 func main() {
 	logChan := make(chan *PassedLogs, 10)
 	cmdQ := make(chan *CommandInfo, 10)
+
+	Init_Server()
 	
 	go Run_server(logChan)
 	go Analyze_Logs(logChan)
